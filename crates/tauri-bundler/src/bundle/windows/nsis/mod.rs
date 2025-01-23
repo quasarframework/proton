711a4727d557d064945eb0c756359d0b064cffe5
Fabian-Lars <github@fabianlars.de>
2025-01-23 13:54:02 +0100

    Update crates/tauri-bundler/src/bundle/windows/nsis/mod.rs

--- crates/tauri-bundler/src/bundle/windows/nsis/mod.rs
+++ crates/tauri-bundler/src/bundle/windows/nsis/mod.rs
@@ -144,7 +144,7 @@ fn try_add_numeric_build_number(version_str: &str) -> anyhow::Result<String> {
   }
 
   log::warn!(
-    "Unable to parse version build metadata. Numeric value expected, received: {}",
+    "Unable to parse version build metadata. Numeric value expected, received: `{}`. This will be replaced with `0` in `VIProductVersion` because Windows requires this field to be numeric.",
     version.build
   );
 
